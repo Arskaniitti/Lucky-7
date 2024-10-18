f5b9dfa25492c92e515fb862d5c6da607c054742
Ari Mononen <ari.mononen@student.hamk.fi>
2024-10-18 22:26:18 +0300

    Peli valmis
    Valmista tuli.

--- Lucky 7/src/App.java
+++ Lucky 7/src/App.java
@@ -25,5 +25,53 @@ public class App {
 
         in.nextLine();
 
+        //WHILE-LOOP alkaa
+        while (saldo > 1){
+
+            saldo = saldo - yksipeli;
+    
+            int randomNumber1 = random.nextInt(10)+1;
+            int randomNumber2 = random.nextInt(10)+1;
+            int randomNumber3 = random.nextInt(10)+1;
+    
+            //Generoi numerot
+            System.out.print("--" + randomNumber1 + "--");
+            System.out.print(randomNumber2 + "--");
+            System.out.println(randomNumber3 + "--");
+    
+    
+            if (randomNumber1 == 7 | randomNumber2 == 7 | randomNumber3 == 7){
+                System.out.println("Yksi oikein! Voitit 3 euroa!");
+                saldo = saldo + yksioikein;
+            }
+    
+            else if (randomNumber1 == 7 & randomNumber2 == 7 |randomNumber1 == 7 & randomNumber3 == 7 | randomNumber2 == 7 & randomNumber3 == 7){
+                System.out.println("Kaksi oikein! Voitit 5 euroa!");
+                saldo = saldo + kaksioikein;
+            }
+    
+            else if (randomNumber1 == 7 & randomNumber2 == 7 & randomNumber3 == 7){
+                System.out.println("KAIKKI OIKEIN! Voitit 10 euroa!");
+                saldo = saldo + kolmeoikein;
+            }
+    
+            else{
+                System.out.println("Ei voittoa!");
+            }
+    
+            //Pelikierros ohi, näyttää saldon ja kysyy pelataanko uudestaan
+            System.out.println("Saldosi: " + saldo);
+            System.out.println("Haluatko pelata uudestaan?");
+            System.out.println("Jos haluat pelata, paina ENTER");
+            System.out.println("Jos et halua pelata, kirjoita E ja paina ENTER");
+            input = in.nextLine();
+            if (input.equalsIgnoreCase("e")){
+                break;
+            }
+            }
+            
+            //Loopista breakattu ulos ->
+            System.out.println("Peli ohi!");
+
     }
 }
