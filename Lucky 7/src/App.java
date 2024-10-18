8838fe00a85939c33af801b885d8ac1facec849f
Ari Mononen <ari.mononen@student.hamk.fi>
2024-10-18 22:22:24 +0300

    Peli aloitettu
    Lisätty muuttujat, tervetuloa-toivotus ja rahamäärän pyytäminen

---
+++ Lucky 7/src/App.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        Random random = new Random();
+
+        String input;
+        int saldo;
+        int yksipeli = 1;
+        int yksioikein = 3;
+        int kaksioikein = 5;
+        int kolmeoikein = 10;
+
+        //Peli alkaa
+        System.out.println("TERVETULOA PELIIN!");
+        System.out.println("Syötä rahamäärä");
+        saldo = in.nextInt();
+        in.nextLine();
+
+    }
+}
